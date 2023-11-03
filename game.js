8e7ca1f012f5e6e4d56f8c6bbfe6b0ea55ca3f3c
Mina <136591388+MinaAl-janabi@users.noreply.github.com>
2023-11-03 12:51:15 +0100

    Update game.js

--- game.js
+++ game.js
@@ -1,45 +1,91 @@
+
 const sudokuPuzzle = [
-    [799529561,
-         227287495,
-          632743983,
-           468258131,
-            989847927,
-             457075708,
-              685163654,
-               524553429,
-                984792186]
+  [5, 3, 4,  , 7,  , 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3,  , 8],
+  [1,  , 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9,  , 6,  , 4, 2, 3],
+  [4, 2,  , 8, 5   , 7, 9, 1],
+  [7, 1, 3,  , 2, 4, 8, 5, 6],
+  [9,  , 1, 5, 3, 7, 2, 8, 4],
+  [2, 8,  , 4, 1, 9,  , 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
 ];
+const soluPuzzle = [
+    [5, 3, 4, 6, 7, 8, 9, 1, 2],
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9],
+  ];
 
-const sudokuGrid = document.getElementById('sudoku-grid');
-const resultText = document.getElementById('result');
+const sudokuGrid = document.getElementById('my-game');
 
-for (let i = 0; i < 9; i++) {
-    const row = document.createElement('td');
+
+function Puzzle() {
+  sudokuGrid.innerHTML = '';
+
+  for (let i = 0; i < 9; i++) {
+    const row = document.createElement('tr');
+
     for (let j = 0; j < 9; j++) {
-        const cell = document.createElement('td');
-        const input = document.createElement('input');
-        input.type = 'number';
-        input.dataset.row = i;
-        input.dataset.col = j;
-        cell.appendChild(input);
-        row.appendChild(cell);
-    }
+      const cell = document.createElement('td');
+      const input = document.createElement('input');
 
+      input.type = 'test';
+      input.dataset.row = i;
+      input.dataset.col = j;
+      input.value = soluPuzzle[i][j];
+
+      cell.appendChild(input);
+      row.appendChild(cell);
+      sudokuGrid.appendChild(row);
+    }
+  }
+
+
+const play = document.getElementById("remove-items");
+play.addEventListener('click', () => {
+    const sudokuPuzzle = [
+        [5, 3, 4,  , 7,  , 9, 1, 2],
+        [6, 7, 2, 1, 9, 5, 3,  , 8],
+        [1,  , 8, 3, 4, 2, 5, 6, 7],
+        [8, 5, 9,  , 6,  , 4, 2, 3],
+        [4, 2,  , 8, 5   , 7, 9, 1],
+        [7, 1, 3,  , 2, 4, 8, 5, 6],
+        [9,  , 1, 5, 3, 7, 2, 8, 4],
+        [2, 8,  , 4, 1, 9,  , 3, 5],
+        [3, 4, 5, 2, 8, 6, 1, 7, 9],
+      ];
+return sudokuPuzzle;
+});
+
+
+const result = document.querySelectorAll('wrong');
+function inputsvalue(){
+    for (let i = 0; i < 9; i++) ;
+    if (input.value!== soluPuzzle);
+    return wrong;
 }
 
-let td =  document.getElementById('sudoku-grid');
 
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            if (typeof (td) != 'undefined' && sudokuPuzzle != sudokuPuzzle){
-                console.log("wrong....")
-            }
+
+const checkButton = document.getElementById('checkButton');
+const input = document.querySelectorAll('sudoku');
+checkButton.addEventListener('click', () => {
+    input.forEach((input) => {
+        const row = input.dataset.row;
+        const col = input.dataset.col;
+        if (input.value !== soluPuzzle[row][col].toString()) {
+            input.style.backgroundColor = 'red';
+        } else {
+            input.style.backgroundColor = 'green';
         }
-    }
+    });
+});
 
-
-
-
-
-
-
+}
+Puzzle();
